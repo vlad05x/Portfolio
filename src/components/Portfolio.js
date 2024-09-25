17d9301c28baca3da32071b7ee8498389027231f
vlad05x <organizm777alf@gmail.com>
2024-09-25 11:34:58 +0300

    Added projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,24 @@
-import React from "react";
-import "../css/style.scss"
+import React, { useState } from "react";
+import "../css/style.scss";
 import trellotionImg from "../images/Trellotion-photo.png";
 import foodDXImg from "../images/FoodDX.png";
 import diGiImg from "../images/Di-Gi_img.png";
 import rgymImg from "../images/RGym.png";
+import BasketballMania from "../images/BasketballMania.png";
+import LanguageLearning from "../images/LanguageLearning.png";
 import lymaImg from "../images/Site_lyma.png";
 import calculatorImg from "../images/icon_calculator.png";
+import MovieAppLogo from "../images/movie-appLogo.png";
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
+
 
 const portfolioData = [
   {
     imgSrc: trellotionImg,
     title: "Trellotion",
-    description: "An application that will help you with saving tasks you need to do.",
+    description:
+      "An application that will help you with saving tasks you need to do.",
     link: "https://vlad05x.github.io/Trellotion/",
   },
   {
@@ -20,6 +27,27 @@ const portfolioData = [
     description:
       "Discover a unique combination of excellent cuisine and attentive service. Come and enjoy a unique gastronomic experience with us.",
     link: "https://vlad05x.github.io/FoodDX/",
+  },
+  {
+    imgSrc: BasketballMania,
+    title: "BasketballMania",
+    description:
+      "An online store entirely dedicated to basketball, where you'll find everything you need from professional gear to accessories for the game. ",
+    link: "https://vlad05x.github.io/basketball-shop/",
+  },
+  {
+    imgSrc: LanguageLearning,
+    title: "Language-learning",
+    description:
+      "An app for learning new words that turns the memorization process into a fun and effective activity.",
+    link: "https://vlad05x.github.io/Language-learning/",
+  },
+    {
+    imgSrc: MovieAppLogo,
+    title: "Movie-app",
+    description:
+      "An app that helps you find movies",
+    link: "https://movie-app-chi-swart.vercel.app/",
   },
   {
     imgSrc: diGiImg,
@@ -50,7 +78,28 @@ const portfolioData = [
   },
 ];
 
+const ITEMS_PER_PAGE = 6;
+
 function Portfolio() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const startIndex = currentPage * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  const currentProjects = portfolioData.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    if (endIndex < portfolioData.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (startIndex > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
   return (
     <div className="block_portfolio" id="portfolio1">
       <h1 className="title">Portfolio</h1>
@@ -61,11 +110,17 @@ function Portfolio() {
         design.
       </p>
       <div className="cards_portfolio">
-        {portfolioData.map((project, index) => (
+        {currentProjects.map((project, index) => (
           <div className="card_portfolio" key={index}>
-            <img src={project.imgSrc} alt={project.title} style={project.title === "Calculator" ? { width: '200px' } : null} />
+            <img
+              src={project.imgSrc}
+              alt={project.title}
+              style={project.title === "Calculator" ? { width: "200px" } : null}
+            />
             <h2 className="title_services">{project.title}</h2>
-            <p className="text_services text_portfolio">{project.description}</p>
+            <p className="text_services text_portfolio">
+              {project.description}
+            </p>
             <a
               className="show_link"
               href={project.link}
@@ -77,9 +132,24 @@ function Portfolio() {
           </div>
         ))}
       </div>
+      <div className="pagination-buttons">
+        <button
+          className="pagination_btn"
+          onClick={handlePrevPage}
+          disabled={startIndex === 0}
+        >
+          <GrFormPrevious />
+        </button>
+        <button
+          className="pagination_btn"
+          onClick={handleNextPage}
+          disabled={endIndex >= portfolioData.length}
+        >
+          <MdOutlineNavigateNext />
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Portfolio;
-
